6c04dced6c9b490e1afebff0aa5c004896f9f0f4
habib4445 <0124habibur@gmail.com>
2025-10-28 12:31:45 +0600

    add another  btn

--- js.js
+++ js.js
@@ -1,6 +1,13 @@
+// option 2
+
 function makeYellow(){
     document.body.style.backgroundColor = 'Yellow';
 }
 function makeGreen(){
     document.body.style.backgroundColor = 'Green';
+}
+// option 3
+const btnMakeBlue = document.getElementById('btn-make-blue');
+btnMakeBlue.onclick = function makeBlue(){
+    document.body.style.backgroundColor='blue';
 }
