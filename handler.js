02b0c22e48afb966e08d1ffd35578e4fb6627ef7
habib4445 <0124habibur@gmail.com>
2025-10-28 16:43:28 +0600

    habib

--- handler.js
+++ handler.js
@@ -1,1 +1,17 @@
-console.log('finally');
+ document.getElementById('btn-update-title')
+        .addEventListener('click',function (){
+            // console.log('btn clicked');
+            const pageTitleElement = document.getElementById('page-title');
+            console.log(pageTitleElement);
+            pageTitleElement.innerText='Updated page title';
+        })
+
+        // login info
+document.getElementById('btn-login')
+.addEventListener('click',function()
+{
+    const userInfoEl=document.getElementById('user-info');
+    userInfoEl.innerText='User logging successfully';
+}
+
+)
